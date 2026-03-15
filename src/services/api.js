1227733b399394870da1ab8620ae1942aa729083
sivanika <sivanika2025@gmail.com>
2026-03-15 15:26:34 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://moral-backend.onrender.com/api",
+  baseURL: "https://moral-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
